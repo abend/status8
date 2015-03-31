6bc43d4fcf9c3d8ef32feb66a449845649a34a51
Sasha Kovar <sasha-git@arcocene.org>
2015-03-31 12:46:33 -0700

    working software

--- blink8/blink8.ino
+++ blink8/blink8.ino
@@ -1,6 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 
+// use "Arduino Uno" board when uploading to femtoduino
+
 #define PIN 5
+#define LED_COUNT 8
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -9,62 +12,55 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
+
+// how many LEDs?
+uint32_t colors[LED_COUNT];
+uint32_t black = strip.Color(0,0,0);
+
 void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-}
-
-void loop() {
-  waves(1500, 60);
-//
-//  blackout();
-//  blinks(5, 200, 30, false);
-//
-//  rainbow(1000);
-//
-//  waves(1500, 12);
-//
-//  rainbow(1000);
-//
-//  blackout();
-//  blinks(5, 200, 30, true);
-}
-
-void waves(int length, int inc) {
-  int count = strip.numPixels();
-  int half_count = count / 2;
-  /* int inc = 20; */
 
   // generate colors
-  uint32_t colors[inc];
-  float step = 256.0 / inc;
-  uint8_t c;
-  for (int i = 0; i < inc; i++) {
-    c = ((int)(i * step)) & 255;
-    colors[i] = strip.Color(c, c, c);
+  float step = 256.0 / LED_COUNT;
+  for (int i = 0; i < LED_COUNT; i++) {
+    uint8_t c = ((int)(i * step)) & 255;
+    colors[i] = Color(c);
   }
 
-  // for a while
-  for (int run = length; run >= 0; run--) {
-    for (int i = 0; i <= half_count; i++) {
-      int offset = (i + run) % inc;
-
-      strip.setPixelColor(i, colors[offset]);
-      strip.setPixelColor(count - i, colors[offset]);
-    }
-
-    strip.show();
-    delay(15);
-  }
+  Serial.begin(9600);
+  Serial.println("Hi"); 
 }
 
+void loop() 
+{
+	int x;
+	String str;
+
+    if (Serial.available() > 0)
+    {
+        x = Serial.parseInt();
+        str = Serial.readStringUntil('\n');
+		str.trim();
+		Serial.println(str + ": " + String(x));
+		if (checkLED(x)) {
+			strip.setPixelColor(x - 1, str == "on" ? colors[x] : black);
+			strip.show();
+		}
+    }
+}
+
+// valid number for the LED?
+boolean checkLED(int i) {
+	return i > 0 && i <= LED_COUNT;
+}
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
